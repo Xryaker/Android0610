6ee95dcef9070876ed5c169ef9804bbf1628b45f
Xryaker <>
2023-02-23 21:26:48 +0200

    -23.02.2023

---
+++ src/test/java/TestChromeAndroid.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.support.PageFactory;
+
+public class TestChromeAndroid extends BaseClass {
+    static WetherObject wetherObject;
+
+    @BeforeClass
+    public static void b() {
+        driver.get("https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%B4%D0%BD%D0%B5%D0%BF%D1%80-303007131");
+        wetherObject = new WetherObject(driver);
+    }
+
+    @Test
+    public void test1() {
+        wetherObject.printTemp();
+    }
+}
---
+++ src/test/java/TestWithFactory.java
@@ -0,0 +1,16 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.support.PageFactory;
+
+public class TestWithFactory extends BaseClass{
+    static WetherPageFactory wetherPageFactory;
+    @BeforeClass
+    public static void b(){
+        driver.get("https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%B4%D0%BD%D0%B5%D0%BF%D1%80-303007131");
+        wetherPageFactory= PageFactory.initElements(driver,WetherPageFactory.class);
+    }
+    @Test
+    public void test(){
+        wetherPageFactory.print();
+    }
+}
